b410f24b92e8a05f68eb1680fb52ee1b0497de5f
KoheyAoyama <kou_713@hotmail.com>
2020-12-04 21:59:19 +0900

    feat(index.js): expose addresses on browser

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const port = 3030
 
 app.get('/', (req,res) => {
     const data = (data) => {
-        res.json(data)
+        console.log(data)
+        const address = data.results[0]
+        res.send(
+            `
+                <p>郵便番号: ${address.zipcode} の住所は...</p>
+                <h1>${address.address1} ${address.address2} ${address.address3}</h1>
+            `
+        )
     }
 
     axios
