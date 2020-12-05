764e355b7b886fa9251bfc82166d312e033d897b
KoheyAoyama <kou_713@hotmail.com>
2020-12-05 10:14:24 +0900

    refactor(response format): response json instead of html

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const { response } = require('express')
 const express = require('express')
 const axios = require('axios').default
 const app = express()
-const port = process.env.PORT || 80
+const port = process.env.PORT || 3030
 
 app.get('/', (req,res) => {
     console.log(req.query.zipcode)
     const data = (data) => {
-        console.log(data)
-        const address = data.results[0]
-        res.send(
-            `
-                <p>郵便番号: ${address.zipcode} の住所は...</p>
-                <h1>${address.address1} ${address.address2} ${address.address3}</h1>
-            `
-        )
+        res.json(data)
+        //const address = data.results[0]
+        // res.send(
+        //     `
+        //         <p>郵便番号: ${address.zipcode} の住所は...</p>
+        //         <h1>${address.address1} ${address.address2} ${address.address3}</h1>
+        //     `
+        // )
     }
 
     if (req.query.zipcode === undefined) {
